aa37ad9d81e2e53928b365a339eae7f465ab2455
Maneesh <maneeshkumaru721@gmail.com>
2026-05-15 03:18:52 +0530

    A small fix

--- back-end/src/services/verifyMail.js
+++ back-end/src/services/verifyMail.js
@@ -22,9 +22,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls:{
-    rejectUnauthorized: false;
-  }
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 const verifyMail = async (mail, otp) => {
